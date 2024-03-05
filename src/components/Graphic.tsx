35f5b12402feb900e061905e88ef075bf23aaed0
Italo Miranda <italo.miranda8635@gmail.com>
2024-03-04 23:49:37 -0300

    adicionado mensagem quando não houver dados para o dashbord

--- src/components/Graphic.tsx
+++ src/components/Graphic.tsx
@@ -4,9 +4,9 @@ import GraphicDoughnut from "./GraphicDoughnut"
 import GraphicInfor from "./GraphicInfor"
 
 const Graphic = ()=>{
-    const [resApiGraphicBar, setresApiGraphicBar] = useState<any>()
-    const [resApiGraphicDoughmut, setResApiGraphicDoughmut] = useState<any>()
-    const [resApiGraphicDoughmutLoss, setResApiGraphicDoughmutLoss] = useState<any>()
+    const [resApiGraphicBar, setresApiGraphicBar] = useState<any>([])
+    const [resApiGraphicDoughmut, setResApiGraphicDoughmut] = useState<any>([])
+    const [resApiGraphicDoughmutLoss, setResApiGraphicDoughmutLoss] = useState<any>([])
     
     const informationApi = async ()=>{
         try{
@@ -15,27 +15,47 @@ const Graphic = ()=>{
             setresApiGraphicBar(resApi.date)
             setResApiGraphicDoughmut(resApi.solds)
             setResApiGraphicDoughmutLoss(resApi.loss)
+
         }catch(err){
             alert('não foi possivel se conectar a api, tente novamente mais tarde')
         }
     }
 
     useEffect(()=>{
+        if (resApiGraphicBar.length) {
+            console.log('nininini')
+        }
+        console.log(resApiGraphicDoughmut)
+        console.log(resApiGraphicBar)
+    }, [resApiGraphicDoughmut, resApiGraphicBar ])
+
+    useEffect(()=>{
         informationApi()
     }, [])
 
     return(
-        <div className="flex justify-center items-center">
-            <div className="flex mt-4 gap-3 bg-graphic container rounded-md">
-                <div className="flex flex-col justify-around items-center">
-                    <GraphicDoughnut solds={resApiGraphicDoughmut} titleGraphic={'Itens mais vendidos'}/>
-                    <GraphicDoughnut solds={resApiGraphicDoughmutLoss} titleGraphic={'Itens com estoque negativado'}/>
+        <>
+            {
+                resApiGraphicDoughmut.length && resApiGraphicBar.length ?
+                <div className="flex justify-center items-center">
+                    <div className="flex mt-4 gap-3 bg-graphic container rounded-md">
+                        <div className="flex flex-col justify-around items-center">
+                            <GraphicDoughnut solds={resApiGraphicDoughmut} titleGraphic={'Itens mais vendidos'}/>
+                            <GraphicDoughnut solds={resApiGraphicDoughmutLoss} titleGraphic={'Itens com maior lucro'}/>
+                        </div>
+                        <div className="flex flex-col w-full">
+                            <GraphicInfor data={resApiGraphicBar}/>
+                        </div>
+                    </div>
                 </div>
-                <div className="flex flex-col w-full">
-                    <GraphicInfor data={resApiGraphicBar}/>
+            :
+                <div className="bg-graphic text-center mt-4 p-4">
+                    <h2>Aqui aparecerão seus dados em dahsbord quando houver algum.</h2>
                 </div>
-            </div>
-        </div>
+            }
+            
+        </>
+
     )
 }
 
